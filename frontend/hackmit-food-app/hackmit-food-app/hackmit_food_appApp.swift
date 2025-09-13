31fa404019d2a37795144336471c825aacf45b0d
Arvindh Manian <arvindh.manian@gmail.com>
2025-09-13 18:42:29 -0400

    switch to swift

---
+++ frontend/hackmit-food-app/hackmit-food-app/hackmit_food_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackmit_food_appApp.swift
+//  hackmit-food-app
+//
+//  Created by Arvindh Manian on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct hackmit_food_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/hackmit-food-app/hackmit-food-appTests/hackmit_food_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  hackmit_food_appTests.swift
+//  hackmit-food-appTests
+//
+//  Created by Arvindh Manian on 9/13/25.
+//
+
+import Testing
+@testable import hackmit_food_app
+
+struct hackmit_food_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
