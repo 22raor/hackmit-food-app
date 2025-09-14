9148d5021bbf7a39ca213cf1ee4db48e5db39f30
Arvindh Manian <arvindh.manian@gmail.com>
2025-09-13 22:55:03 -0400

    pwa

---
+++ frontend/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
